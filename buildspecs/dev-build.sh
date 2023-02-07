98b91b78de03dde9a4e426ac05fc58683b32b255
Caleb Courier <caleb.courier@gmail.com>
2023-02-07 11:24:19 -0600

    explicitly use bash instead of ubuntu default dash

---
+++ buildspecs/dev-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+callerIdentity=$(aws sts get-caller-identity);
+accountId=$(jq -r .Account <<< $callerIdentity);
+version=$(cat ./package.json | jq -r .version);
+appName=$(cat ./package.json | jq -r .name);
+commitSha=$(git rev-parse HEAD);
+region="${AWS_REGION:-us-east-1}";
+ecrEndpoint="${accountId}.dkr.ecr.${region}.amazonaws.com";
+ecrImageUrl="${ecrEndpoint}/${appName}";
+
+docker login -u AWS -p $(aws ecr get-login-password --region $region) $ecrEndpoint;
+
+docker build --build-arg NODE_ENV=production -t "$appName:$commitSha" -t "$appName:$version" -t "$appName:latest" . || exit 1;
+
+docker image tag "$appName:$commitSha" "$ecrImageUrl:$commitSha";
+docker image tag "$appName:$version" "$ecrImageUrl:$version";
+docker image tag "$appName:latest" "$ecrImageUrl:latest";
+
+docker push $ecrImageUrl --all-tags;;
