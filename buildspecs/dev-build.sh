c2fabf52a2d7485db61a64a48739999ac5f0add5
Caleb Courier <caleb.courier@gmail.com>
2023-02-07 13:49:26 -0600

    consolidate build and push, create driver

--- buildspecs/dev-build.sh
+++ buildspecs/dev-build.sh
@@ -11,10 +11,13 @@ ecrImageUrl="${ecrEndpoint}/${appName}";
 
 docker login -u AWS -p $(aws ecr get-login-password --region $region) $ecrEndpoint;
 
-docker buildx build --platform linux/amd64,linux/arm64 --progress plain -t "$appName:$commitSha" -t "$appName:$version" -t "$appName:latest" . || exit 1;
+docker buildx create --use --name crossx;
 
-docker image tag "$appName:$commitSha" "$ecrImageUrl:$commitSha";
-docker image tag "$appName:$version" "$ecrImageUrl:$version";
-docker image tag "$appName:latest" "$ecrImageUrl:latest";
-
-docker push $ecrImageUrl --all-tags;
+docker buildx build \
+  --push \
+  --platform linux/amd64,linux/arm64 \
+  --progress plain \
+  -t "$ecrImageUrl:$commitSha" \
+  -t "$ecrImageUrl:$version" \
+  -t "$ecrImageUrl:latest" . \
+  || exit 1;
