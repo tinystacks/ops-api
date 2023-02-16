01871f357b0e0e09013353dac8dfda8ac1edcc24
nefertitirogers <nefertitirogers@gmail.com>
2023-02-16 12:26:48 -0800

    use WidgetParser instead of GenericWidget

--- src/controllers/widget-controller.ts
+++ src/controllers/widget-controller.ts
@@ -1,18 +1,18 @@
 import { Widget as WidgetType } from '@tinystacks/ops-model';
 import WidgetClient from '../clients/widget-client';
 // temporary until parser is done and can return instances of plugin classes
-import { GenericWidget } from '@tinystacks/ops-core';
+import { WidgetParser } from '@tinystacks/ops-core';
 
 const WidgetController = {
   async getWidget (consoleName: string, widgetId: string): Promise<WidgetType> {
     return (await WidgetClient.getWidget(consoleName, widgetId)).toJson();
   },
   async postWidget (consoleName: string, createWidgetBody: WidgetType): Promise<WidgetType> {
-    const widget = GenericWidget.fromJson(createWidgetBody);
+    const widget = WidgetParser.fromJson(createWidgetBody);
     return (await WidgetClient.createWidget(consoleName, widget)).toJson();
   },
   async putWidget (consoleName: string, widgetId: string, updateWidgetBody: WidgetType): Promise<WidgetType> {
-    const widget = GenericWidget.fromJson(updateWidgetBody);
+    const widget = WidgetParser.fromJson(updateWidgetBody);
     widget.id = widgetId;
     return (await WidgetClient.updateWidget(consoleName, widgetId, widget)).toJson();
   },
