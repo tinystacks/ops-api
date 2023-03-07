090a972b2b83416331537229773f1fabccb51b0c
Caleb Courier <caleb.courier@gmail.com>
2023-03-07 13:44:10 -0600

    fix route file location

--- src/routes/consoles/{consoleName}.ts
+++ src/routes/consoles/{consoleName}.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import ConsoleController from '../../../controllers/console-controller.js';
+import ConsoleController from '../../controllers/console-controller.js';
 
 export default function () {
   return {
